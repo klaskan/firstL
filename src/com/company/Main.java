347fcf1282047061c00e16c9c4941f5a65cd66ad
Klas Rogne Kanestrøm <klas.kanestrom@gmail.com>
2018-01-09 13:13:41 +0100

    commit 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,7 +12,13 @@ public class Main {
             list.add(i);
         }
 
-       
+        ArrayList<TestKlasse> elever = new ArrayList<>();
+        elever.add(new TestKlasse("Klas", 10));
+        elever.add(new TestKlasse("Ine", 15));
+
+        System.out.println(elever.get(0).getNavn());;
+
+
 
 
 
--- src/com/company/TestKlasse.java
+++ src/com/company/TestKlasse.java
@@ -1,4 +1,20 @@
 package com.company;
 
 public class TestKlasse {
+
+    private String navn;
+    private int alder;
+
+    public TestKlasse(String navn, int alder) {
+        this.navn = navn;
+        this.alder = alder;
+    }
+
+    public String getNavn() {
+        return navn;
+    }
+
+    public int getAlder() {
+        return alder;
+    }
 }
